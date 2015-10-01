cb04cc18f6b98c1491016958813a0f46c63723db
Hannes Tydén <hannes@tyden.name>
2015-10-02 00:08:08 +0200

    [alphabet-cipher] Solve encoding

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,23 @@
 (ns alphabet-cipher.coder)
 
+(defn alphabet []
+  (map char (range (int \a) (+ (int \z) 1))))
+
+(defn pos [c]
+  (- (int c) (int \a)))
+
+(defn chart [r c]
+  (nth (alphabet) (mod (+ (pos r) (pos c)) 26)))
+
+(defn padded-keyword [k m]
+  (subs
+    (apply str (concat (repeat (/ (count m) (count k)) k)))
+    0
+    (count m)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+    (map chart (seq (padded-keyword keyword message)) (seq message))))
 
 (defn decode [keyword message]
   "decodeme")

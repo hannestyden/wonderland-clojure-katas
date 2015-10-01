b14b720b9d7bdddd95dfc154a73168e6e5fe65de
Hannes Tydén <hannes@tyden.name>
2015-10-02 01:51:20 +0200

    [alphabet-cipher] Solve decipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -12,20 +12,33 @@
 (defn de-chart [c r]
   (nth (alphabet) (mod (- (pos r) (pos c)) 26)))
 
-(defn padded-keyword [k m]
-  (subs
-    (apply str (concat (repeat (/ (count m) (count k)) k)))
-    0
-    (count m)))
+(defn dec-chart [c r]
+  (nth (alphabet) (mod (- (pos c) (pos r)) 26)))
+
+(defn r-take [s n]
+  (take n s))
+
+(defn pad [string n]
+  (subs (apply str (concat (repeat (/ n (count string)) string))) 0 n))
+
+(defn generator [s]
+  (take 1
+    (filter
+      (fn [x] (= (pad x (count s)) (apply str s)))
+      (map
+        (partial apply str)
+        (map
+          (partial r-take s)
+          (range 1 (+ 1 (count s))))))))
 
 (defn encode [keyword message]
   (apply str
-    (map en-chart (seq (padded-keyword keyword message)) (seq message))))
+    (map en-chart (seq (pad keyword (count message))) (seq message))))
 
 (defn decode [keyword message]
   (apply str
-    (map de-chart (seq (padded-keyword keyword message)) (seq message))))
+    (map de-chart (seq (pad keyword (count message))) (seq message))))
 
 (defn decypher [cypher message]
-  "decypherme")
-
+  (apply str
+    (generator (map dec-chart (seq cypher) (seq message)))))
